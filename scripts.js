d1f504dad22d1b1a44f3147803506d5a546a70b3
Dubymar Tollinchi <dubymarjtr@gmail.com>
2021-03-24 13:45:18 -0500

    Add TODO for form submition

--- scripts.js
+++ scripts.js
@@ -5,4 +5,6 @@ const form = document.querySelector("form");
 form.addEventListener("submit", (event) => {
   event.preventDefault();
   console.log("submit");
+
+  // TODO: get the name from the form and add them to 'students'
 });
