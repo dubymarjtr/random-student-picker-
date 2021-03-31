5879d03896719664a0b56d5aa31e127de490f64e
Dubymar Tollinchi <dubymarjtr@gmail.com>
2021-03-31 13:45:15 -0500

    delete TODO comment

--- scripts.js
+++ scripts.js
@@ -5,11 +5,9 @@ const form = document.querySelector("form");
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
-  // TODO: get the name from the form and add them to 'students'
   const newStudent = Object.fromEntries(new FormData(event.target));
 
   students.push(newStudent);
-  console.log(students);
 });
 
 // Part of homework 9
