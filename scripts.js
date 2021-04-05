8b6d56b73229029cd06fa2d85f4fc8c087fc0e77
Dubymar Tollinchi <dubymarjtr@gmail.com>
2021-04-05 13:24:32 -0500

    create function to get random int

--- scripts.js
+++ scripts.js
@@ -2,6 +2,12 @@ const students = [];
 
 const form = document.querySelector("form");
 
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
