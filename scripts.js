f015e711fd4b655eb4cfeb5e6193421dc9d8c0ae
Dubymar Tollinchi <dubymarjtr@gmail.com>
2021-04-05 12:39:48 -0500

    Delete homework 9 part

--- scripts.js
+++ scripts.js
@@ -9,19 +9,3 @@ form.addEventListener("submit", (event) => {
 
   students.push(newStudent);
 });
-
-// Part of homework 9
-// document.querySelector("#fname").addEventListener("focus", (e) => {
-//   e.preventDefault();
-//   console.log("Entered input!");
-// });
-
-// document.querySelector("#lname").addEventListener("blur", (e) => {
-//   e.preventDefault();
-//   console.log("Left input!");
-// });
-
-// document.querySelector("form").addEventListener("submit", (e) => {
-//   e.preventDefault();
-//   console.log("Submitted form!");
-// });
